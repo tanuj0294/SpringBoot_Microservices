1cf419987851a05e24cc21b3bb31409687a11e83
tanuj0294 <tanuj0294@gmail.com>
2018-03-03 20:36:20 +0530

    basic folder layout

---
+++ src/main/java/org/springboot/microservices/learn/microservices/MicroServicesApplication.java
@@ -0,0 +1,12 @@
+package org.springboot.microservices.learn.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServicesApplication.class, args);
+	}
+}
